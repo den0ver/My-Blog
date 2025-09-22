1eb5543f7b8bc87887a7862e8ee717568ba52011
den0ver <dlalernen@gmail.com>
2025-09-22 10:49:10 +0200

    Added commenting system

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -22,7 +23,9 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, publish__year=year, publish__month=month, 
     publish__day=day, slug=post, status=Post.Status.PUBLISHED)
-    context = {'post': post}
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    context = {'post': post, 'form': form, 'comments': comments}
     return render(request, 'blog/post/detail.html', context)
 
 
@@ -44,3 +47,16 @@ def post_share(request, post_id):
         form = EmailPostForm()
     context = {'post': post, 'sent': sent, 'form': form}
     return render(request, 'blog/post/share.html', context)
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    context = {'post': post, 'comment': comment, 'form': form}
+    return render(request, 'blog/post/comment.html', context)
