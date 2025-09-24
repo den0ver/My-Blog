ef5d97812d7b8934d55b58df5045f117d2af4960
den0ver <dlalernen@gmail.com>
2025-09-24 11:41:07 +0200

    Added full-text search to the blog

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('<slug:tag_slug>/tag/', views.post_list, name='post_list_by_tag'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 
 def post_list(request, tag_slug=None):
@@ -69,3 +70,17 @@ def post_comment(request, post_id):
         comment.save()
     context = {'post': post, 'comment': comment, 'form': form}
     return render(request, 'blog/post/comment.html', context)
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (Post.published.annotate(search=SearchVector('title', 'body'))).filter(search=query)
+    context = {'form': form, 'query': query, 'results': results}
+    return render(request, 'blog/post/search.html', context)
